b5273984b99f4478cc6d88d3ff04c07153e8ac0b
BJL <ben@workhelix.com>
2022-11-20 17:45:12 -0800

    fix app placement

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 import os.path as op
 import sys
-import streamlit as st
-from app.pages._home import run
+from st_pages import show_pages_from_config
 
 sys.path.append(op.abspath(op.join(op.dirname(__file__), "..")))
 
-from st_pages import show_pages_from_config
+from app.pages._home import run
 
 show_pages_from_config()
 
